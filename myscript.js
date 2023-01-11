86b9be0c5e28089de6710a4dd2a4e13260c92361
rabbitlopez77 <rabbitlopez22@gmail.com>
2023-01-10 23:36:35 -0800

    created the function to add booknumbers

--- myscript.js
+++ myscript.js
@@ -1,16 +1,33 @@
 const theLibrary = [];
+let bookName = '';
+let bookAuthor = '';
+let bookPages = '';
+let haveRead = '';
+let bookCount = 0;
 const bookFactory = (name, author, pages, read) => {
     return{name,author, pages, read}
 }
-const theHobbit = bookFactory('The Hobbit', 'JRR Tolkien', 150, 'Yes')
-
-document.getElementById('theAuthor').innerHTML = theHobbit.author;
-document.getElementById('theBookPages').innerHTML = theHobbit.pages;
-document.getElementById('haveRead').innerHTML = theHobbit.read;
-
-const addBook = () => {
-    let bookName = document.querySelector('#bookName').value;
-    let bookAuthor = document.querySelector('#bookAuthor').value;
-    let bookPages = document.querySelector('#bookPages').value
-    document.getElementById('theBookName').innerHTML = bookName;
+const bookInput = () => {
+    bookName = document.querySelector('#bookName').value
+    document.querySelector('#bookName').value = '';
+  bookAuthor = document.querySelector('#bookAuthor').value
+    document.querySelector('#bookAuthor').value = '';
+  bookPages = document.querySelector('#bookPages').value
+    document.querySelector('#bookPages').value = '';
+  if(document.getElementById('haveRead').checked == true) {   
+      haveRead = 'I Have Read This';   
+      } else {  
+      haveRead = 'I Have Not Read';   
+      } 
 }
+const numberOfBooks= () => {
+    bookCount++
+    bookNumber = 'book' + bookCount
+    return bookNumber;
+}
+const addBook = () => {
+    bookInput();
+    numberOfBooks();
+    console.log(bookNumber)
+}
+
