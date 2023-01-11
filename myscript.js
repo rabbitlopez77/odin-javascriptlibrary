b274abb1feb186b6b4c10bcf3f1752805f268b2d
rabbitlopez77 <rabbitlopez22@gmail.com>
2023-01-11 00:16:11 -0800

    have the function down

--- myscript.js
+++ myscript.js
@@ -4,6 +4,8 @@ let bookAuthor = '';
 let bookPages = '';
 let haveRead = '';
 let bookCount = 0;
+let bookNumber = ''
+
 const bookFactory = (name, author, pages, read) => {
     return{name,author, pages, read}
 }
@@ -25,10 +27,31 @@ const numberOfBooks= () => {
     bookNumber = 'book' + bookCount
     return bookNumber;
 }
+const addToLibrary = (book) => {
+    theLibrary.push(book)
+}
+const showAllBooks = () => {
+    let totalBooksName = theLibrary.map(a => a.name);
+      document.getElementById('theBookName').innerHTML = totalBooksName;
+    let totalBookAuthors = theLibrary.map(a => a.author)
+      document.getElementById('theAuthor').innerHTML = totalBookAuthors;
+    let totalBookPages = theLibrary.map(a => a.pages);
+      document.getElementById('theBookPages').innerHTML = totalBookPages;
+    let totalBookRead = theLibrary.map(a => a.read)
+    document.getElementById('read').innerHTML = totalBookRead;
+}
+
 const addBook = () => {
     bookInput();
     numberOfBooks();
-    console.log(bookNumber);
-    
+    let newBook =bookNumber;
+    newBook = bookFactory(bookName, bookAuthor, bookPages, haveRead);
+    addToLibrary(newBook);
+    showAllBooks();
 }
+
+
+
+
+
 
